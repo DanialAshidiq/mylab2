bbd97125568522d41d9ff777063ee45d95a0e745
DanialAshidiq <77533235+DanialAshidiq@users.noreply.github.com>
2022-03-23 12:23:23 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask , request , jsonify
 from main import chatWithBot
+
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
+@app.route('/chat')
+def chat():
     chatInput = request.form['chatInput']
     # unicodedata.normalize('NFKD', chatInput).encode('ascii', 'ignore')
-    return '<h1>' + jsonify(chatBotReply=chatWithBot(chatInput))  + '<h1>'
+    return '<h1>' + jsonify(chatBotReply=chatWithBot(chatInput))  + '</h1>'
+@app.route('/index')
+def index():
+    return '<h1> HEROKU </h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
