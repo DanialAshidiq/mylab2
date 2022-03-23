c11eac03b9cc23425fa66a1038367e2ca27a7b77
DanialAshidiq <77533235+DanialAshidiq@users.noreply.github.com>
2022-03-23 12:21:28 +0800

    update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     chatInput = request.form['chatInput']
     # unicodedata.normalize('NFKD', chatInput).encode('ascii', 'ignore')
-    return jsonify(chatBotReply=chatWithBot(chatInput))
+    return '<h1>' + jsonify(chatBotReply=chatWithBot(chatInput))  + '<h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
